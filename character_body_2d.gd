bc840337374763bde26ec9f5a749d2379ef58747
OnlyNumber404 <agustinusdenny110@gmail.com>
2025-01-13 15:37:11 +0700

    third

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,11 +1,30 @@
 extends CharacterBody2D
-@export var speed = 400
 var hp = 100
 
+const speed = 400
+const accel = 1500
+const friction = 600
+
+var input = Vector2.ZERO
+
+func _physics_process(delta):
+	player_movement(delta)
+
 func get_input():
-	var input_direcction = Input.get_vector("Left","Right","Up","Down")
-	velocity = input_direcction * speed
+	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	input.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	return input.normalized()
 	
-func _physics_process(delta: float) -> void:
-	get_input()
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
+	else :
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(speed)
+		
 	move_and_slide()
